79c425735f8468620532f5a55fd77589e4ecfd3a
dilasedetmen <sedetmendila@gmail.com>
2022-07-01 14:10:52 +0200

    unnecessary assets removed

--- src/home.js
+++ src/home.js
@@ -1,11 +1,4 @@
 function home() {
-  const lettuce = document.createElement("img");
-  const parsley = document.createElement("img");
-  const tomato = document.createElement("img");
-  const garlic = document.createElement("img");
-  const carolina = document.createElement("img");
-  const beans = document.createElement("img");
-
   const homecontainer = document.createElement("div");
   const tabContent = document.createElement("div");
 
@@ -27,36 +20,10 @@ function home() {
   text.classList = "text";
   text.textContent = " We brought Italy to you!\r\n\r\n Come visit us!";
 
-  lettuce.classList = "bg-item-lettuce";
-  lettuce.src = "./assets/bg-items/bg-lettuce.png";
-
-  parsley.classList = "bg-item-parsley";
-  parsley.src = "./assets/bg-items/bg-parsley.png";
-
-  tomato.classList = "bg-item-tomato";
-  tomato.src = "./assets/bg-items/bg-tomato.png";
-
-  garlic.classList = "bg-item-garlic";
-  garlic.src = "./assets/bg-items/bg-garlic.png";
-
-  carolina.classList = "bg-item-carolina";
-  carolina.src = "./assets/bg-items/bg-redpepper.png";
-
-  beans.classList = "bg-item-beans";
-  beans.src = "./assets/bg-items/bg-beans.png";
-
   homecontainer.id = "home";
   homecontainer.classList = "active";
   homecontainer.dataset.tabContent = "";
-  homecontainer.append(
-    // lettuce,
-    // parsley,
-    // tomato,
-    // garlic,
-    // carolina,
-    // beans,
-    textContainer
-  );
+  homecontainer.append(textContainer);
 
   tabContent.classList = "tab-content";
   tabContent.append(homecontainer);
